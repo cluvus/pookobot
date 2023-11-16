a401a77b72d5fce7b64c814dab141d2281b6017f
Victor Hugo <149340273+cluvus@users.noreply.github.com>
2023-11-16 19:02:45 +0000

    +

---
+++ Comandos/Moderação/clear.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "clear",
+    description: "Limpe o canal de texto",
+    type: Discord.ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'quantidade',
+            description: 'Insira um número de mensagens para serem excluidas.',
+            type: Discord.ApplicationCommandOptionType.Number,
+            required: true,
+        }
+    ],
+
+    run: async (client, interaction) => {
+
+        let numero = interaction.options.getNumber('quantidade')
+
+        if (!interaction.member.permissions.has(Discord.PermissionFlagsBits.ManageMessages)) {
+            interaction.reply({ content: `❌ | Você não tem permissão para utilizar este comando.`, ephemeral: true })
+        } else {
+
+            if (parseInt(numero) > 200 || parseInt(numero) <= 0) {
+
+                interaction.reply({ content: `❌ | Insira um número de mensagens para serem apagadas! Lembrando que tem que ser um número de \`1 á 99\``, ephemeral: true})
+
+            } else {
+
+                interaction.channel.bulkDelete(parseInt(numero))
+
+                interaction.reply(`**Chat limpo com sucesso!!!**\n\n♻ **| Faxineiro:** ${interaction.user}\n\n🧹 **| Mensagens Limpas:** \`${numero}\``)
+
+                let apagar_mensagem = "nao" // sim ou nao
+
+                if (apagar_mensagem === "sim") {
+                    setTimeout(() => {
+                        interaction.deleteReply()
+                    }, 5000)
+                } else if (apagar_mensagem === "nao") {
+                    return;
+                }
+
+            }
+
+        }
+
+    }
+}
--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -2,6 +2,7 @@ const client = require("../index");
 const Canvas = require('canvas')
 const Discord = require('discord.js')
 const path = require('path')const image = require('../data/image.json')
+const fs = require ('fs')
 
 //Registrar as fontes
 const { registerFont } = require('canvas');
@@ -15,8 +16,13 @@ client.on('guildMemberAdd', async (member) => {
 
     const canvas = Canvas.createCanvas(1920, 1080);
     const ctx = canvas.getContext('2d')
+    
+const random = Math.floor(Math.random() * image.banner.length);
 
-    const background = await Canvas.loadImage(image['banner2'])
+    const date = new Date();
+    const brazilTime = date.toLocaleTimeString('pt-BR');
+    
+    const background = await Canvas.loadImage(image.banner[random])
 
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
 
@@ -28,30 +34,63 @@ client.on('guildMemberAdd', async (member) => {
     const x = (canvas.width - 400) / 2;
     const y = (canvas.height - 400) / 2;
 
-    ctx.font = '160px ts';
+    ctx.font = '180px ts';
     ctx.fillStyle = '#fff'
+    ctx.strokeStyle = "black";
+ctx.lineWidth = 8;
+   ctx.strokeText(`BEM-VINDO(A)`, 500, 900)
     const welcomeText = `BEM-VINDO(A)`
-    ctx.fillText(welcomeText, 520, 900)
+    ctx.fillText(welcomeText, 500, 900)
 
     ctx.font = '90px ccg';
     ctx.fillStyle = '#fff'
+    ctx.strokeStyle = "black";
+   ctx.lineWidth = 6;
     const memberTagText = `${member.user.tag}`
     const memberTagTextWidth = ctx.measureText(memberTagText).width;
+    ctx.strokeText(memberTagText,x + (400 - memberTagTextWidth) / 2, y + 660)
     ctx.fillText(memberTagText, x + (400 - memberTagTextWidth) / 2, y + 660)
     
     ctx.font = '70px anc';
-    ctx.fillStyle = '#dedede'
-    const nmrText = `#${member.guild.memberCount}`
-    ctx.fillText(nmrText, 1720, 120)
+ ctx.fillStyle = '#fff'
+        ctx.strokeStyle = "black";
+ctx.lineWidth = 8;
+  const nmrText = `#${member.guild.memberCount}`
+  ctx.strokeText(`${nmrText}`, 1720, 100)
+    ctx.fillText(nmrText, 1720, 100)
 
-
-    ctx.beginPath()
-    ctx.arc(x + 400 / 2, y + 400 / 2, 220, 0, Math.PI * 2);
+    ctx.font = '70px anc';
+    ctx.fillStyle = '#fff'
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 8;
+     const brazilDate = date.toLocaleDateString('pt-BR', { day: 'numeric', month: 'numeric' });
+     ctx.strokeText(`${brazilDate}`, 30, 100)
+     ctx.fillText(brazilDate, 30, 100)
+    
+   
+   ctx.beginPath()
+     ctx.arc(x + 400 / 2, y + 400 / 2 - 40, 255, 0, Math.PI * 2);
     ctx.closePath()
-    ctx.clip()
+    ctx.fillStyle = '#dedede'
+    ctx.fill()
 
+// Desenhar o arco no canvas
+ctx.beginPath();
+ctx.arc(x + 400 / 2, y + 400 / 2 - 40, 250, 0, Math.PI * 2); // Diminuir o valor de y em 20
+ctx.closePath();
+ctx.clip();
+
+// Desenhar a imagem no canvas
+ctx.drawImage(avatar, 960 - 250, 540-250 - 40, 500, 500); // Ajustar as coordenadas da imagem
+
+
+
+    //ctx.beginPath()
+ //ctx.arc(x + 400 / 2, y + 400 / 2, 220, 0, Math.PI * 2);
+     //ctx.closePath()
+    //ctx.clip()
       
-ctx.drawImage(avatar, 960 - 220, 540-220, 440, 440)
+ //ctx.drawImage(avatar, 960 - 220, 540-220, 440, 440)
 
     const attachment = new Discord.AttachmentBuilder(await canvas.toBuffer(), {name: 'welcome.png'})
 
